7cdecb48177f3ebeeb00591a1f1c4bd06dfb4003
RealColorix <nicholas.skoda@gmail.com>
2024-12-21 21:38:49 +0100

    Functioning items. Added branch and pointy stick

--- src/main/java/net/colorixer/item/ModItems.java
+++ src/main/java/net/colorixer/item/ModItems.java
@@ -4,16 +4,21 @@ import net.colorixer.TougherThanLlamas;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
 
 public class ModItems {
 
-    public static final Item POINTY_STICK = registerItem("pointy_stick", new Item(new Item.Settings()));
+    public static final Item POINTY_STICK = registerItem("pointy_stick", Item::new, new Item.Settings());
+    public static final Item BRANCH = registerItem("branch", Item::new, new Item.Settings());
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of("ttll", name), item);
+    public static Item registerItem(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TougherThanLlamas.MOD_ID, path));
+        return Items.register(registryKey, factory, settings);
     }
 
     public static void registerModItems() {
@@ -21,6 +26,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(POINTY_STICK);
+            entries.add(BRANCH);
         });
     }
 }
