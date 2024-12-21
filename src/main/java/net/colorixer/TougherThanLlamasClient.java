f24f98d07c6e1ffd349611710419c2ec7f0c9fc0
RealColorix <nicholas.skoda@gmail.com>
2024-12-21 10:43:04 +0100

    Your message here

---
+++ src/main/java/net/colorixer/TougherThanLlamasClient.java
@@ -0,0 +1,14 @@
+package net.colorixer;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TougherThanLlamasClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
